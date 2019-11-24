945957fdcf6779b950d0ce9a0b46f7a26c225fa1
Cerberus Security <43178175+cerberusec@users.noreply.github.com>
2019-11-24 09:30:40 -0800

    Update runas.ps1

--- runas.ps1
+++ runas.ps1
@@ -1,4 +1,6 @@
+# powershell -ExecutionPolicy Bypass -File runas.ps1
+
 $secpasswd = ConvertTo-SecureString "PASSWORD" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("USERNAME", $secpasswd)
 $computer = "TARGET"
-[System.Diagnostics.Process]::Start("C:\Windows\System32\cmd.exe"),"", $mycreds.Username, $mycreds.Password, $computer)
+[System.Diagnostics.Process]::Start("C:\Windows\System32\cmd.exe","", $mycreds.Username, $mycreds.Password, $computer)
